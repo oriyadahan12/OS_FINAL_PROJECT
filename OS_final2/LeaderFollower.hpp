fc4b3a09ed01216641a16e5186af72ed23670b7b
Shira Yogev <54641048+May2303@users.noreply.github.com>
2024-10-17 21:07:42 +0300

    Add files via upload

---
+++ OS_final2/LeaderFollower.hpp
@@ -0,0 +1,33 @@
+#ifndef LFP_HPP
+#define LFP_HPP
+
+#include <iostream>
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <vector>
+#include <functional>
+
+using namespace std;
+
+class LF {
+    private:
+        void worker(int id);  // Worker function
+        vector<thread> threads;  // Vector to store threads
+        vector<int> threadIDs;  // Vector to store thread IDs
+        queue<function<void()>> taskQueue;  // Queue to store tasks
+        mutex queueMutex;  // Mutex to protect the tasks queue
+        mutex stopMutex;  // Mutex to protect the stop flag
+        condition_variable condition;  // used to notify the threads that there is a task in the queue
+        bool stopFlag;  // Flag to stop the threads if set to true
+        int leader;  // Leader thread
+    public:
+        LF(int num_threads);
+        ~LF();
+        void addTask(function<void()> task);  // Add a task to the queue
+        void start();
+        void stop();
+};
+
+#endif // LFP_HPP
