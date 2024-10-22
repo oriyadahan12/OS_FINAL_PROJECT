8e3cfcd37dcdb405781efe9ff31eaa17385c1305
shirel1233 <155564582+shirel1233@users.noreply.github.com>
2024-10-22 22:47:20 +0300

    Update PAO.hpp

--- OSProject/PAO/PAO.hpp
+++ OSProject/PAO/PAO.hpp
@@ -13,29 +13,39 @@
 
 
 class PAO {
-    private:
+public:
+    // Constructor: Accepts a list of functions to be executed by worker threads
+    PAO(const std::vector<std::function<void(void*)>>& functions);
 
-        // Worker struct that will represents a worker thread
-        struct Worker {
-            std::thread* thread;  // The worker thread
-            std::function<void(void*)> function;  // The function that the worker will execute
-            std::queue<void*> taskQueue;  // The queue of tasks for the worker. task = void* because the function can get any type of task
-            std::mutex* queueMutex;  // Mutex for the task queue
-            std::condition_variable* condition;  // Condition variable for the worker to wait on
-            std::queue<void*>* nextTaskQueue;  // Pointer to the next worker's task queue
-        };
-
-        void workerFunction(Worker& worker, Worker* nextWorker);
-
-        std::vector<Worker> workers;
-        std::atomic<bool> stopFlag;   // atomic flag to stop the threads
+    // Destructor: Cleans up resources and ensures threads are stopped
+    ~PAO();
     
-    public:
-        PAO(const std::vector<std::function<void(void*)>>& functions);  // the constructor get the functions to be executed by the workers
-        ~PAO();
-        PAO() = default;
+    // Default constructor (in case we need it)
+    PAO() = default;
 
-        void addTask(void* task);
-        void start();
-        void stop();
+    // Adds a task to be executed by the workers
+    void addTask(void* task);
+    
+    // Starts the worker threads and begins processing tasks
+    void start();
+
+    // Signals the workers to stop processing and shuts down the threads
+    void stop();
+
+private:
+    // Worker struct: Represents an individual worker thread and its associated data
+    struct Worker {
+        std::thread* thread;  // Pointer to the thread running the worker
+        std::function<void(void*)> function;  // Function that the worker will execute on tasks
+        std::queue<void*> taskQueue;  // Queue of tasks assigned to the worker (tasks are of generic type `void*`)
+        std::mutex* queueMutex;  // Mutex for synchronizing access to the worker's task queue
+        std::condition_variable* condition;  // Condition variable to notify the worker of new tasks
+        std::queue<void*>* nextTaskQueue;  // Pointer to the task queue of the next worker in the pipeline
+    };
+
+    // Function that defines the behavior of a worker thread. Processes tasks and passes them to the next worker.
+    void workerFunction(Worker& worker, Worker* nextWorker);
+
+    std::vector<Worker> workers;  // Vector holding all worker threads
+    std::atomic<bool> stopFlag;   // Atomic flag used to signal workers to stop
 };
